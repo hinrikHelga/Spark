a83e8dc53eae50b719e178c5424923dbac0cdd4e
Hinrik Helgason <hihe@trifork.com>
2023-09-28 12:16:13 +0200

    init

---
+++ Spark/SparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkApp.swift
+//  Spark
+//
+//  Created by Hinrik Helgason on 28/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
