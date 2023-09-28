13053d3c2e3151722c996b0b0bac2e84420d1d7c
Hinrik Helgason <hihe@trifork.com>
2023-09-28 14:16:49 +0200

    Sending image to server

--- Spark/ContentView.swift
+++ Spark/ContentView.swift
@@ -39,6 +39,55 @@ struct ContentView: View {
                     self.sourceType = .photoLibrary
                     self.isImagePickerDisplay.toggle()
                 }.padding()
+                
+                if let selectedImage = selectedImage {
+
+                    Button(action: {
+                        // convert image into base 64
+                        
+                        let uiImage: UIImage = selectedImage
+                        let imageData: Data = uiImage.jpegData(compressionQuality: 0.1) ?? Data()
+                        let imageStr: String = imageData.base64EncodedString()
+                                                
+                        guard let encoded = "https://hackathon2023-spark.vercel.app/api/img".addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed) else {
+                            print("encoded")
+                            return
+                        }
+                        
+                        // send request to server
+                        guard let myURL = URL(string: encoded) else {
+                            print("invalid URL")
+                            return
+                        }
+                        
+                        // create parameters
+                        let paramStr: String = "image=\(imageStr)"
+                        let paramData: Data = paramStr.data(using: .utf8) ?? Data()
+                        
+                        var urlRequest: URLRequest = URLRequest(url: myURL)
+                        urlRequest.httpMethod = "POST"
+                        urlRequest.httpBody = paramData
+                        
+                        // required for sending large data
+                        urlRequest.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+                        
+                        // send the request
+                        URLSession.shared.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
+                            guard let data = data else {
+                                print("invalid data")
+                                return
+                            }
+                            
+                            // show response in string
+                            let responseStr: String = String(data: data, encoding: .utf8) ?? ""
+                            print(responseStr)
+                        })
+                        .resume()
+                        
+                    }, label: {
+                        Text("Upload image")
+                    }).padding()
+                }
             }
             .navigationBarTitle("Spark")
             .sheet(isPresented: self.$isImagePickerDisplay) {
